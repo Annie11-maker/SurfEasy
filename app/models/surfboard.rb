0724b817272c342ffcdf0998a4881fa167dd2747
quratulain shouket <quratulainshouket11@gmail.com>
2025-01-20 16:59:10 +0000

    SurfEasy 20/01 5.59 pm

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -1,4 +1,8 @@
 class Surfboard < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :size, presence: true
+  validates :color, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :surfboards, :bookings
+  has_many :surfboards
+  has_many :bookings
+
+  validates :user_name, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
