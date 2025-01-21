56d04c4384d20c3c445057fac50921e83e4a6342
quratulain shouket <quratulainshouket11@gmail.com>
2025-01-21 12:22:49 +0000

    SurfEasy 21/1 12.22 pm

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   # get 'bookings/index'
   # get 'bookings/show'
   # get 'bookings/create'
@@ -12,7 +13,7 @@ Rails.application.routes.draw do
   # get 'surfboards/update'
   # get 'surfboards/destroy'
   devise_for :users
-  root to: "surfboards#index"
+  root to: "pages#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
