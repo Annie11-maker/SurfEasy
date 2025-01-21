3ce54e06bcafd0ee596074266152f39788e48723
quratulain shouket <quratulainshouket11@gmail.com>
2025-01-21 17:34:20 +0000

    SurfEasy 21/1 5.34 pm

--- app/controllers/surfboards_controller.rb
+++ app/controllers/surfboards_controller.rb
@@ -1,10 +1,6 @@
 class SurfboardsController < ApplicationController
   def index
-<<<<<<< HEAD
-    @surfboard = Surfboard.all
-=======
     @surfboards = Surfboard.all
->>>>>>> master
   end
 
   def show
