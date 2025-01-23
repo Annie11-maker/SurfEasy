fc24e2193cc969fc9781285077c3340f4c23b9cf
Sanju Varkey <5578483+chief10sguru@users.noreply.github.com>
2025-01-23 14:27:32 +0000

    Booking Status Show All Working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,11 +2,11 @@ class BookingsController < ApplicationController
 
   def index
     @bookings = Booking.all
-
   end
 
   def show
     @booking = Booking.find(params[:id])
+    @bookings = Booking.all
   end
 
   def create
