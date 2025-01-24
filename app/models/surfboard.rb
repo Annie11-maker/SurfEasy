0f990df2c8e5405d9404351f6f4b6c146edfea59
quratulain shouket <quratulainshouket11@gmail.com>
2025-01-24 08:42:16 +0000

    SurfEasy 24/1 8.42 AM

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -3,13 +3,13 @@ class Surfboard < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :bookings
   has_one_attached :photo
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  # validates :name, presence: true
-  # validates :description, presence: true, length: { maximum: 500 }
-  # validates :size, presence: true
-  # validates :color, presence: true
-  # validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :name, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :size, presence: true
+  validates :color, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
