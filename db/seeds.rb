523b32e402a54453c7287fc355f808b4e2317618
Gabriel Atak <machokatak@yahoo.com>
2025-01-24 12:47:25 +0000

    Added Surfboards Photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,52 +26,52 @@ marco = User.create!(user_name: 'Money Marco', first_name: "Marco", last_name: "
 puts 'Creating Surfboards...'
 Surfboard.create!(
   name: 'Classic Longboard',
-  image_url: 'https://www.wetsuitcentre.co.uk/media/catalog/product/cache/157c811d2fe3ff1b7ea8f84c248170d8/s/c/screenshot_2023-11-03_104735.png',
+  image_url: 'https://plus.unsplash.com/premium_photo-1667865667770-9cd722cf6d57?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   description: 'A versatile 9-foot longboard for your daily surfing needs.',
   price: 499.99,
   user: david,
-  color: "tan",
-  size: "large"
+  color: "Tan",
+  size: "Large"
 )
 
 Surfboard.create!(
   name: 'Speedy Shortboard',
-  image_url: 'https://www.wetsuitcentre.co.uk/media/catalog/product/cache/60dfce8a96edaa221a78ed63232f76c9/s/c/screenshot_2023-10-06_074419.png',
+  image_url: 'https://img.freepik.com/free-photo/front-view-surfboard-beach_23-2148723380.jpg?ga=GA1.1.1978007662.1737587384&semt=ais_hybrid',
   description: 'A fast and agile 6-foot shortboard designed for quick turns and speed.',
   price: 599.99,
   user: san,
-  color: "white",
-  size: "large"
+  color: "White",
+  size: "Large"
 )
 
 Surfboard.create!(
   name: 'Retro Fish',
-  image_url: 'https://www.wetsuitcentre.co.uk/media/catalog/product/cache/8f56e7d1ddebb659e06c9d4c2d205225/e/l/elements-longboard-ftu-navy-base-1.jpg',
+  image_url: 'https://img.freepik.com/free-photo/surfboard-sand_1252-569.jpg?t=st=1737721495~exp=1737725095~hmac=5380aaa27be98d45e9697492f849d33a3285d6b746dd483addd95f7cda84a1e7&w=740',
   description: 'A retro-style fish surfboard with a twin fin setup.',
   price: 450.00,
   user: michelle,
-  color: "blue",
-  size: "large"
+  color: "Yellow",
+  size: "Large"
 )
 
 Surfboard.create!(
   name: 'Big Wave Gun',
-  image_url: 'https://www.wetsuitcentre.co.uk/media/catalog/product/cache/3f90f119a98e62b8b8ce03290dc74a28/s/c/screenshot_2024-05-29_085929.png',
+  image_url: 'https://images.unsplash.com/photo-1588605800921-78dc67c62685?q=80&w=2922&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   description: 'An 8-foot board designed for tackling the biggest waves.',
   price: 750.50,
   user: annie,
-  color: "red",
-  size: "short"
+  color: "White",
+  size: "Short"
 )
 
 Surfboard.create!(
   name: 'Funboard Hybrid',
-  image_url: 'https://www.wetsuitcentre.co.uk/media/catalog/product/cache/3abfbba11a25b8481a53bcc54efb7c72/s/p/spark_mal_1.jpg',
+  image_url: 'https://images.unsplash.com/photo-1531722569936-825d3dd91b15?q=80&w=4470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   description: 'A versatile fun board that blends elements of a longboard and shortboard.',
   price: 550.75,
   user: gabriel,
-  color: "gray",
-  size: "medium"
+  color: "Orange",
+  size: "Medium"
 )
 
 Surfboard.create!(
@@ -80,8 +80,8 @@ Surfboard.create!(
   description: 'A perfect board for beginners with a soft top for safety.',
   price: 350.00,
   user: marco,
-  color: "white",
-  size: "short"
+  color: "Green",
+  size: "Short"
 )
 
 # 3. Display a message 🎉
